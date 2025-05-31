dd850e0120d2297311e9ccc85c0df9ccf2d86ae7
Parth Vakharia <Peranoxx@gmail.com>
2025-05-31 16:11:26 +0530

    add vue project

---
+++ resources/js/App.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+      <router-view></router-view>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'App',
+  };
+  </script>
+  
+  <style>
+  nav {
+    padding: 1rem;
+  }
+  nav a {
+    margin: 0 0.5rem;
+    text-decoration: none;
+    color: #42b983;
+  }
+  nav a.router-link-exact-active {
+    font-weight: bold;
+  }
+  </style>
+  
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
-import './bootstrap';
+import { createApp } from 'vue';
+import router from './router/index.js'; // import router
+import App from './App.vue'; // main app component
+
+const app = createApp(App);
+
+app.use(router);
+
+app.mount('#app');
